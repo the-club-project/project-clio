863bbe6be7d5e651c7091137838164116a3067af
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-06-19 11:04:05 +0800

    Added two way communication PUB/REP

--- main.lua
+++ main.lua
@@ -6,11 +6,18 @@ local unistd = require("posix.unistd")
 local battery = require("components.battery")
 local bluetooth = require("components.bluetooth")
 
-local ipc_path = "ipc:///tmp/hypr_bridge.ipc"
+local ipc_path = "ipc:///tmp/clio_bridge.ipc"
+local res_path = "ipc:///tmp/clio_bridge_res.ipc"
 
 local context = zmq.context()
 local publisher = context:socket(zmq.PUB)
 assert(publisher:bind(ipc_path))
+
+local responder = context:socket(zmq.REP)
+assert(responder:bind(res_path))
+
+local poller = zmq.poller(1)
+poller:add(responder, zmq.POLLIN)
 
 signal.signal(signal.SIGINT, function()
     publisher:close()
@@ -20,6 +27,15 @@ signal.signal(signal.SIGINT, function()
 end)
 
 while true do
+    local ready = poller:poll(0)
+    if ready > 0 then
+        local msg = responder:recv()
+        local req = json.decode(msg)
+        print(msg)
+        local reply = {received = true}
+        responder:send(json.encode(reply))
+    end
+
     local capacity, status, e_full, e_full_design, p_now, e_now = battery.info()
     local powered, connected, d_name, s_results = bluetooth.info()
 
